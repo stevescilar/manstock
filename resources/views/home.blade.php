20e4b1288f241b5fdab97de2d166a3002b6ddb3d
stevescilar <stevemuambi@gmail.com>
2023-03-02 18:33:37 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,7 +42,7 @@
     
             <div class="section-title">
               <h2>Services</strong></h2>
-              <p>We  Offer The Services in Town</p>
+              <p>We  Offer The Services in Towned</p>
             </div>
     
             <div class="row">
